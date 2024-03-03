edb2c647e61350935407bc7a429f65a737719f24
KyoungHyun <102707496+Oneourbefore@users.noreply.github.com>
2024-03-03 21:46:42 +0900

    Update and rename testing2.py to cosinesim.py

--- cosinesim.py
+++ cosinesim.py
@@ -1,14 +1,10 @@
 import tensorflow_hub as hub
 import tensorflow_text
 
-# Universal Sentence Encoder 모델 로드
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
-
-# 비교할 두 문장
 sentence1 = "The quick brown fox jumps over the lazy dog."
 sentence2 = "A fast brown fox leaps over a lazy hound."
 
-# 문장을 임베딩하여 벡터로 변환
 embeddings = embed([sentence1, sentence2])
 
 # 두 벡터 간의 코사인 유사도 계산
